7455c3f5134dc6319fbda94cef41cee0a7458c59
ghYoon93 <gh.yoon93@gmail.com>
2020-01-15 23:25:00 +0900

    Modify WindowTest.java

--- 10_interface/src/interface_1/WindowTest.java
+++ 10_interface/src/interface_1/WindowTest.java
@@ -34,11 +34,13 @@ public class WindowTest extends Frame implements MouseListener, MouseMotionListe
 	}
 	
 	public void mouseClicked(MouseEvent e) {
+		Graphics g = getGraphics();
+		g.drawString("X:"+ e.getX() + "\t Y:+e.getY() ", e.getX(), e.getY());
 //		System.out.println("마우스 클릭");
 //		System.out.println("X:"+ e.getX() + "\t Y: "+e.getY());
-		this.x = e.getX();
-		this.y = e.getY();
-		repaint();  // update() -> paint() 순으로 호출
+//		this.x = e.getX();
+//		this.y = e.getY();
+//		repaint();  // update() -> paint() 순으로 호출
 	}
 	
 	
@@ -52,6 +54,6 @@ public class WindowTest extends Frame implements MouseListener, MouseMotionListe
 	
 	
 	public void paint(Graphics g) {
-		g.drawString("X:" + this.x+"\t Y: "+this.y, x,y);
+//		g.drawString("X:" + this.x+"\t Y: "+this.y, x,y);
 	}
 }
